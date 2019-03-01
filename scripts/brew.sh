6c97a78915fdccc1f5b084ae7d233fcae8dbac11
Robert Parris <rparris@afs.net>
2019-03-01 09:43:26 -0500

    less busy brew script. more fun

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -42,59 +42,18 @@ brew install gnupg
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
-brew install screen
-brew install homebrew/php/php56 --with-gmp
+brew install python3
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
 brew install rename
-brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
 brew install thefuck
+brew install lolcat
+brew install cowsay
+brew install bat
+brew install tmux
 
 # install some extra git tools
 brew tap PotatoLabs/homebrew-git-redate
@@ -113,3 +72,6 @@ source ~/.bashrc
 
 # install node
 nvm install node
+
+# edicat for edi files
+pip3 install -U edicat
