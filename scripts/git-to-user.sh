f7ab8e392465c43e4fd9cf4a387bb751c41369b2
Robert Parris <rwparris2@gmail.com>
2020-10-27 13:42:49 -0400

    update profile scripts

---
+++ scripts/git-to-user.sh
@@ -0,0 +1,7 @@
+cp .gitconfig                   ~/.gitconfig
+cp .gitconfig-tetria.conf       ~/.gitconfig-tetria.conf
+cp .gitconfig-personal.conf     ~/.gitconfig-personal.conf
+cp .gitignore_global            ~/.gitignore_global
+cp .zshrc                       ~/.zshrc
+cp .zprofile                    ~/.zprofile
+cp .p10k.zsh                    ~/.p10k.zsh
